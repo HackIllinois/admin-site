ea5d9d2a82e70a98213582367cd29e0a04449b71
Jonathan Xu <jonathanj.xu97@gmail.com>
2018-07-28 10:28:37 -0700

    Create React App Initialization

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Admin Portal</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
