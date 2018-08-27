83b84a0f1770f826ae641beb9f47eb3c1e81ec28
Jonathan Phipps <jonathanmp25@gmail.com>
2018-08-27 16:52:02 -0500

    Switch to cli json-server

--- mockServer.js
+++
@@ -1,9 +0,0 @@
-const jsonServer = require('json-server');
-const path = require('path');
-const server = jsonServer.create();
-const router = jsonServer.router('mockData.json');
-
-server.use(router);
-server.listen(3000, () => {
-  console.log('JSON Server is running');
-});
