b34f7403985ac228311b1014e0c7cf10843a01f5
Jonathan Phipps <jonathanmp25@gmail.com>
2018-08-29 16:44:07 -0500

    Set up redux and store

--- src/index.js
+++ src/index.js
@@ -2,6 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore, combineReducers } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import checkinReducer from './services/checkin/reducer';
+import eventsReducer from './services/events/reducer';
+import registrationReducer from './services/registration/reducer';
+import rsvpReducer from './services/rsvp/reducer';
+
+const rootReducer = combineReducers({
+  checkin: checkinReducer,
+  events: eventsReducer,
+  registration: registrationReducer,
+  rsvp: rsvpReducer,
+});
+
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
 registerServiceWorker();
