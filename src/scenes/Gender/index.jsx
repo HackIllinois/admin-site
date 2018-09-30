97f7efa89b6d01de0fd9f8fc7ca1f47280d486d2
brianwang1217 <brianwang1217@gmail.com>
2018-09-29 22:48:38 -0500

    updated graph to include diff demographics

--- src/scenes/Gender/index.jsx
+++ src/scenes/Gender/index.jsx
@@ -6,16 +6,52 @@ const Gender = () => (
   <ResponsiveBar
     data={[
       {
-          gender: 'Male \u2642',
-          num: 82
+          "gender": 'Male \u2642',
+          "num": 82,
+          "grad2019": 25,
+          "grad2020": 27,
+          "grad2021": 15,
+          "grad2022": 15
       },
       {
-          gender: 'Female \u2640',
-          num: 75
+          "gender": 'Female \u2640',
+          "num": 75,
+          "grad2019": 17,
+          "grad2020": 23,
+          "grad2021": 20,
+          "grad2022": 15
       },
       {
-          gender: 'Non-Binary',
-          num: 20
+          "gender": 'Non-Binary',
+          "num": 20,
+          "grad2019": 5,
+          "grad2020": 7,
+          "grad2021": 6,
+          "grad2022": 2
+      }
+    ]}
+    defs={[
+      {
+        id: 'grad',
+        type: 'linearGradient',
+        colors: [
+          { offset: 0, color: '#f7ecc8' },
+          { offset: 100, color: '#edd074' },
+        ],
+      }
+    ]}
+    fill={[
+      {
+        "match": {
+            "id": "grad2020"
+        },
+        "id": "grad"
+      },
+      {
+        "match": {
+            "id": "grad2022"
+        },
+        "id": "grad"
       }
     ]}
     margin={{
@@ -31,13 +67,13 @@ const Gender = () => (
         "legendOffset":-50
     }}
     indexBy="gender"
-    keys={['num']}
+    keys={["grad2019", "grad2020", "grad2021", "grad2022"]}
     padding={0.5}
     colors="pastel1"
     colorBy="index"
     labelTextColor="#57767b"
     labelSkipWidth={16}
-    labelSkipHeight={16}
+    labelSkipHeight={13}
   />
 );
 
