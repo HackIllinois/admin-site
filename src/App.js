1367dec5545cdc8bda1e1cb0c67ad624b2760b0d
Krzysztof Dutka <kdutka2@illinois.edu>
2018-10-03 20:55:23 -0500

    Tabs Within a grid element (#17)
    * Inserted the tabs into a grid-element and improved the css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+
+import './styles.css';
+
 
 import School from './components/School';
 
@@ -8,6 +12,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+      
         <h1 className="admin-Header">Admin Portal</h1>
 
         <div className="div-List grid">
@@ -33,9 +38,25 @@ class App extends Component {
           <div className="grid-element">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Non arcu risus quis varius quam quisque id diam. Cras fermentum odio eu feugiat pretium. Id consectetur purus ut faucibus pulvinar elementum. Morbi tristique senectus et netus et malesuada fames ac. Tristique risus nec feugiat in fermentum posuere urna. Elementum sagittis vitae et leo duis ut. In fermentum et sollicitudin ac orci phasellus egestas tellus rutrum.
           </div>
-
+      
           <div className="grid-element">
             <School />
+          </div>
+      
+          <div className="grid-element grid-element-tabs">
+            <Tabs>
+              <TabList>
+                <Tab>Tab 1</Tab>
+                <Tab>Tab 2</Tab>
+                <Tab>Tab 3</Tab>
+                <Tab>Tab 4</Tab>
+              </TabList>
+
+              <TabPanel>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</TabPanel>
+              <TabPanel>Non arcu risus quis varius quam quisque id diam. Cras fermentum odio eu feugiat pretium. Id consectetur purus ut faucibus pulvinar elementum. </TabPanel>
+              <TabPanel>Morbi tristique senectus et netus et malesuada fames ac. Tristique risus nec feugiat in fermentum posuere urna.</TabPanel>
+              <TabPanel>Elementum sagittis vitae et leo duis ut. In fermentum et sollicitudin ac orci phasellus egestas tellus rutrum.</TabPanel>
+            </Tabs>
           </div>
         </div>
       </div>
