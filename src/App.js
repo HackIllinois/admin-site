0d1909e63311fd8463d550a03e040092125a7b86
Brian Yang <brian.yang328@gmail.com>
2018-10-10 20:58:20 -0500

    Add react router to admin portal with separate login page

--- src/App.js
+++ src/App.js
@@ -1,65 +1,20 @@
 import React, { Component } from 'react';
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import './styles.css';
-
-
-import School from './components/School';
+import Home from './scenes/Home';
+import Login from './scenes/Login'
 
 import './styles.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      
-        <h1 className="admin-Header">Admin Portal</h1>
-
-        <div className="div-List grid">
-          <div className="grid-element">
-            <div>
-              This is the Admin Page of Hack Illinois 2019. This page is in progress.
-            </div>
-            <span className="login">
-              <div>
-                <input placeholder="User Name" />
-              </div>
-              <div>
-                <input placeholder="Password" />
-              </div>
-            </span>
-          </div>
-
-          <div className="grid-element">
-            Feb 22-24, 2019<br/>
-            Maybe announcements here or something
-          </div>
-
-          <div className="grid-element">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Non arcu risus quis varius quam quisque id diam. Cras fermentum odio eu feugiat pretium. Id consectetur purus ut faucibus pulvinar elementum. Morbi tristique senectus et netus et malesuada fames ac. Tristique risus nec feugiat in fermentum posuere urna. Elementum sagittis vitae et leo duis ut. In fermentum et sollicitudin ac orci phasellus egestas tellus rutrum.
-          </div>
-      
-          <div className="grid-element">
-            <School />
-          </div>
-      
-          <div className="grid-element grid-element-tabs">
-            <Tabs>
-              <TabList>
-                <Tab>Tab 1</Tab>
-                <Tab>Tab 2</Tab>
-                <Tab>Tab 3</Tab>
-                <Tab>Tab 4</Tab>
-              </TabList>
-
-              <TabPanel>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</TabPanel>
-              <TabPanel>Non arcu risus quis varius quam quisque id diam. Cras fermentum odio eu feugiat pretium. Id consectetur purus ut faucibus pulvinar elementum. </TabPanel>
-              <TabPanel>Morbi tristique senectus et netus et malesuada fames ac. Tristique risus nec feugiat in fermentum posuere urna.</TabPanel>
-              <TabPanel>Elementum sagittis vitae et leo duis ut. In fermentum et sollicitudin ac orci phasellus egestas tellus rutrum.</TabPanel>
-            </Tabs>
-          </div>
-        </div>
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/login" component={Login} />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
