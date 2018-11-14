2521bfb7a815e219ee26fc2fac0b541396a8fd80
Jonathan Xu <jonathanj.xu97@gmail.com>
2018-11-14 16:39:48 -0600

    API Integration (#43)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
+import Auth from './components/Auth';
 import MenuAppBar from './components/MenuAppBar';
 import Menu from './components/Menu';
 
@@ -17,6 +18,7 @@ class App extends Component {
           <div>
             <MenuAppBar />
             <Menu />
+            <Auth />
             <Switch>
               <Route exact path="/" component={Home} />
               <Route path="/login" component={Login} />
--- src/index.js
+++ src/index.js
@@ -1,31 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
 import { applyMiddleware, createStore, combineReducers } from 'redux';
 import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+import authReducer from './services/auth/reducer';
+import { setJWT } from './services/auth/actions';
 import checkinReducer from './services/checkin/reducer';
 import eventsReducer from './services/events/reducer';
 import registrationReducer from './services/registration/reducer';
 import rsvpReducer from './services/rsvp/reducer';
 import sessionReducer from './services/ui/reducer';
+import userReducer from './services/user/reducer';
 
 import './reset.css';
 
 const rootReducer = combineReducers({
+  auth: authReducer,
   checkin: checkinReducer,
   events: eventsReducer,
   registration: registrationReducer,
   rsvp: rsvpReducer,
   session: sessionReducer,
+  user: userReducer
 });
 
 const store = createStore(
   rootReducer,
-  applyMiddleware(logger)
+  applyMiddleware(thunk, logger)
 );
+
+// Use admin token from environment variables in development
+// since OAuth won't work with local api instance
+if (process.env.NODE_ENV === "development") {
+  store.dispatch(setJWT(process.env.REACT_APP_TOKEN));
+}
 
 ReactDOM.render(
   <Provider store={store}>
