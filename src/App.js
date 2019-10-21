6f764d5dd83d074988b02eccff81940c1fac6a73
Abhinav Pappu <abhi1pappu9@gmail.com>
2019-10-21 00:28:39 -0500

    Add navigation menu

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faColumns, faUsers, faBell, faCalendar } from '@fortawesome/free-solid-svg-icons';
+
+import './app.scss';
+import Dashboard from './components/Dashboard';
+
+const routes = [
+  { path: '/', name: "Dashboard", icon: faColumns },
+  { path: '/users', name: "Users", icon: faUsers },
+  { path: '/notifications', name: "Notifications", icon: faBell },
+  { path: '/events', name: "Events", icon: faCalendar },
+]
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter className="router">
+        <div className="navigation-menu-container">
+          <div className="navigation-menu">
+            {
+              routes.map(route => (
+                <NavLink to={route.path} exact className="navigation-link" activeClassName="active">
+                  <FontAwesomeIcon icon={route.icon} fixedWidth/>&nbsp; {route.name}
+                </NavLink>
+              ))
+            }
+          </div>
+        </div>
+
         <Switch>
-          <Route exact path="/"></Route>
+          <Route exact path="/">
+            <Dashboard/>
+          </Route>
           <Route path="/users"></Route>
           <Route path="/notifications"></Route>
           <Route path="/events"></Route>
