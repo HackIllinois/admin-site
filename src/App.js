322164790d9b7549a615c59b8b7d66ca8088e2fc
Abhinav Pappu <abhi1pappu9@gmail.com>
2019-10-20 19:12:53 -0500

    Add basic router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/"></Route>
+          <Route path="/users"></Route>
+          <Route path="/notifications"></Route>
+          <Route path="/events"></Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
