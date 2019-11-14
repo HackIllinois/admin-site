406466abd74fa77cbd8378f6710d7b831db0d434
Abhinav Pappu <abhi1pappu9@gmail.com>
2019-11-14 17:04:45 -0600

    Copy api.js from main repo and remove unnecessary methods

---
+++ src/api.js
@@ -0,0 +1,69 @@
+const API = 'https://api.hackillinois.org';
+
+function headers() {
+  return {
+    Authorization: sessionStorage.getItem('token'),
+    'Content-Type': 'application/json',
+  };
+}
+
+function request(method, endpoint, body) {
+  return fetch(API + endpoint, {
+    method,
+    headers: headers(),
+    body: JSON.stringify(body),
+  }).then(res => {
+    if (res.ok) {
+      return res.json();
+    }
+    throw Error(res);
+  });
+}
+
+export function isAuthenticated() {
+  return sessionStorage.getItem('token');
+}
+
+export function authenticate(to) {
+  if (process.env.REACT_APP_TOKEN) {
+    sessionStorage.setItem('token', process.env.REACT_APP_TOKEN);
+  } else {
+    to = `${process.env.REACT_APP_URL}/auth/?to=${to}`;
+    to = `${API}/auth/github/?redirect_uri=${to}`;
+  }
+  window.location.replace(to);
+}
+
+export function getToken(code) {
+  return request('POST', '/auth/code/github/', { code });
+}
+
+export function getRoles() {
+  return request('GET', '/auth/roles/')
+    .then(res => res.roles);
+}
+
+export function getApplication() {
+  return request('GET', '/registration/attendee/');
+}
+
+export function uploadResume(resume) {
+  return request('GET', '/upload/resume/upload/')
+    .then(res => res.resume)
+    .then(url => fetch(url, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/pdf' },
+      body: resume,
+    }))
+    .then(res => {
+      if (res.ok) {
+        return res;
+      }
+      throw Error(res);
+    });
+}
+
+export function apply(isEditing, application) {
+  const method = isEditing ? 'PUT' : 'POST';
+  return request(method, '/registration/attendee/', application);
+}
