9dd3fd60ca99ed736335bf853cf9202a4fffb6fc
Abhinav Pappu <abhi1pappu9@gmail.com>
2019-12-28 21:30:24 -0600

    Make navigation menu mobile friendly

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route, NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faColumns, faUsers, faBell, faCalendar } from '@fortawesome/free-solid-svg-icons';
+import { faColumns, faUsers, faBell, faCalendar, faBars } from '@fortawesome/free-solid-svg-icons';
 
 import './App.scss';
 import PrivateRoute from './components/PrivateRoute';
@@ -18,44 +18,70 @@ const routes = [
   { path: '/events', name: "Events", icon: faCalendar },
 ]
 
-function App() {
-  return (
-    <div className="App">
-      <BrowserRouter className="router">
-        <div className="navigation-menu-container">
-          <div className="navigation-menu">
-            {
-              routes.map(route => (
-                <NavLink exact to={route.path} className="navigation-link" activeClassName="active" key={route.name}>
-                  <FontAwesomeIcon icon={route.icon} fixedWidth/>&nbsp; {route.name}
-                </NavLink>
-              ))
-            }
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      menuOpen: false,
+    };
+  }
+
+  render() {
+    const { menuOpen } = this.state;
+    return (
+      <div className="App">
+        <BrowserRouter className="router">
+          <div className="menu-open-button" onClick={() => this.setState({ menuOpen: true })}>
+            <FontAwesomeIcon icon={faBars} fixedWidth/>
+            <div className="highlight"/>
           </div>
-        </div>
 
-        <Switch>
-          <Route path="/auth" component={Auth}></Route>
-          
-          <PrivateRoute exact path="/">
-            <Dashboard/>
-          </PrivateRoute>
-          
-          <PrivateRoute path="/users">
-            <Users/>
-          </PrivateRoute>
-
-          <PrivateRoute path="/events">
-            <Events/>
-          </PrivateRoute>
-
-          <PrivateRoute path="/notifications">
-            <Notifications/>
-          </PrivateRoute>
-        </Switch>
-      </BrowserRouter>
-    </div>
-  );
+          <div
+            className={'menu-background' + (menuOpen ? ' open' : '')}
+            onClick={() => this.setState({ menuOpen: false })}/>
+  
+          <div className={'navigation-menu-container' + (menuOpen ? ' open' : '')}>
+            <div className="navigation-menu">
+              {
+                routes.map(route => (
+                  <NavLink
+                    exact
+                    to={route.path}
+                    className="navigation-link"
+                    activeClassName="active"
+                    onClick={() => this.setState({ menuOpen: false })}
+                    key={route.name}>
+                      <FontAwesomeIcon icon={route.icon} fixedWidth/>
+                      <span className="text">&nbsp; {route.name}</span>
+                  </NavLink>
+                ))
+              }
+            </div>
+          </div>
+  
+          <Switch>
+            <Route path="/auth" component={Auth}></Route>
+            
+            <PrivateRoute exact path="/">
+              <Dashboard/>
+            </PrivateRoute>
+            
+            <PrivateRoute path="/users">
+              <Users/>
+            </PrivateRoute>
+  
+            <PrivateRoute path="/events">
+              <Events/>
+            </PrivateRoute>
+  
+            <PrivateRoute path="/notifications">
+              <Notifications/>
+            </PrivateRoute>
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
