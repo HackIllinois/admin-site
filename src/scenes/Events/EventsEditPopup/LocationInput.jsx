6d2cd0b956dc11bb8442fe8b95571d03dda9a4c1
Abhinav Pappu <abhi1pappu9@gmail.com>
2020-02-16 14:28:49 -0600

    Add support for event location tags

--- src/scenes/Events/EventsEditPopup/LocationInput.jsx
+++ src/scenes/Events/EventsEditPopup/LocationInput.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
-import Checkbox from 'components/Checkbox';
 import './LocationInput.scss';
+import Checkbox from 'components/Checkbox';
+import { StyledSelect } from 'components/SelectField';
 
 const locations = [
   {
@@ -26,15 +27,27 @@ const locations = [
   }
 ];
 
+const tags = ['DCL', 'KENNEY', 'SIEBEL0', 'SIEBEL1', 'SIEBEL2', 'ECEB1', 'ECEB2', 'ECEB3'];
+
+const blankLocation = { description: '', latitude: 0, longitude: 0, tags: [] };
+
 export default function LocationCheckbox({ field, form }) {
   // Note: The description of a selectedLocation may include additional details ("DCL Basement and 1st floor")
   // while the description of the corresponding location would only contain "DCL"
 
   const selectedLocations = field.value || [];
 
-  const addLocation = (location, details = '') => {
+  const addLocation = (location, otherInfo = {}) => {
+    // otherInfo may be only updating a single field (e.g. tags) so we set the current
+    // values of the other fields as the defaults
+    const {
+      details = getDetails(location),
+      tags = getTags(location)
+    } = otherInfo;
+
     const newLocation = Object.assign({}, location);
     newLocation.description += ` ${details}`;
+    newLocation.tags = tags;
 
     // First remove any existing selectedLocations corresponding to this location (for editing details)
     form.setFieldValue(field.name, selectedLocations
@@ -53,15 +66,18 @@ export default function LocationCheckbox({ field, form }) {
     selectedLocations.some(selected => selected.description.includes(location.description))
   );
 
+  const findSelectedLocation = name =>
+    selectedLocations.find(({ description }) => description.includes(name)) || blankLocation;
+
   const getDetails = location => {
     // Find the corresponding selectedLocation's description (contains extra details) for this location
-    const selectedDescription = selectedLocations
-      .map(selected => selected.description)
-      .find(description => description.includes(location.description));
+    const selectedDescription = findSelectedLocation(location.description).description;
 
     // Remove the base location name to only return the details
-    return (selectedDescription || '').replace(location.description, '').trimStart();
+    return selectedDescription.replace(location.description, '').trimStart();
   }
+
+  const getTags = location => findSelectedLocation(location.description).tags;
 
   return (
     <div className="location-input">
@@ -79,7 +95,21 @@ export default function LocationCheckbox({ field, form }) {
               placeholder="Details"
               value={getDetails(location)}
               disabled={!isSelected(location)}
-              onChange={e => addLocation(location, e.target.value)}
+              onChange={e => addLocation(location, { details: e.target.value })}
+            />
+
+            <StyledSelect
+              className="tags-select"
+              placeholder="Tags"
+              value={getTags(location).map(tag => ({ value: tag, label: tag }))}
+              options={tags.map(tag => ({ value: tag, label: tag }))}
+              onChange={selected => addLocation(location, { tags: (selected || []).map(x => x.value) })}
+              isDisabled={!isSelected(location)}
+              menuPlacement="top"
+              isMulti={true}
+              controlShouldRenderValue={false}
+              hideSelectedOptions={false}
+              isClearable={false}
             />
           </div>
         ))
