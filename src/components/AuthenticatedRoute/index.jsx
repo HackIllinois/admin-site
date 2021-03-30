3ec33fe86b10a147db0d43ef0199163f6b682404
Abhinav Pappu <abhi1pappu9@gmail.com>
2021-03-29 22:58:50 -0500

    Fix authentication for local development

--- src/components/AuthenticatedRoute/index.jsx
+++ src/components/AuthenticatedRoute/index.jsx
@@ -2,9 +2,16 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import { isAuthenticated, authenticate, getRoles, getUserId } from 'util/api';
 
+// TODO: make distinction between google auth and github auth clearer
+
+// checks if the user's token matches the desired provider
+// (also passes if REACT_APP_TOKEN is set because that indicates local development,
+// so there's only one token and we don't get to choose which provider we want)
+const isDesiredAuthentication = (provider) =>  getUserId().startsWith(provider) || !!process.env.REACT_APP_TOKEN;
+
 const AuthenticatedRoute = ({ path, provider = 'google', ...props }) => {
   // make sure user is authenticated, and that their authentication provider matches the desired one
-  if (!isAuthenticated() || (!getUserId().startsWith(provider))) {
+  if (!isAuthenticated() || !isDesiredAuthentication(provider)) {
     authenticate(path, provider);
     return <div>Loading</div>;
   }
